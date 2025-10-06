6bc81baec754a07ce6169fa7da0f77ce2a1deb12
rOyalFruit <ekdeon@gmail.com>
2025-10-06 17:07:07 +0900

    ch 7-2까지 예제 코드

---
+++ ch07/02_bank_access_error/02_bank_access_error.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class bank {
+private:
+	int safe; //금고
+
+public:
+	bank(); // 기본 생성자
+	void use_counter(int _in, int _out); // 입출금 창구 함수
+};
+
+int main() {
+	bank my_bank;
+	my_bank.safe -= 100;
+
+	return 0;
+}
